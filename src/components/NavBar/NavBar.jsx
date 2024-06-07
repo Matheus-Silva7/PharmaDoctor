51066cd41b663032ccc94fd92b1b239858e39c1f
Matheus-Silva7 <mate.almeidasilva@gmail.com>
2024-06-07 09:51:33 -0300

    navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import iconPharma from "/icon-pharma.svg";
 import "./NavBarStyles.css";
 import { Link } from "react-router-dom";
 import { CiMenuFries } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 
 const NavBar = () => {
+
+  const [active, setActive] = useState(false)
+  const handleClick = () => {
+    setActive(!active)
+  }
+
   return (
     <nav>
       <div className="logo">
         <img src={iconPharma} alt="icon pharma" />
         <p>Pharma</p>
       </div>
-      <ul className="nav-itens">
+      <ul className={active? "nav-itens active":"nav-itens"}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">services</Link></li>
@@ -19,8 +26,11 @@ const NavBar = () => {
         <li><Link to="/contact">Contact</Link></li>
       </ul>
       <a href="" className="call-action">Appointment</a>
-      <div className="menu-mobile">
-        <CiMenuFries size={30} />
+      <div className="menu-mobile" onClick={handleClick}>
+        {
+          active ? (<IoMdClose  size={30} />):(<CiMenuFries size={30} />) 
+        }
+
       </div>
     </nav>
   );
