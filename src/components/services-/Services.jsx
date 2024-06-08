ebc8ef92d16a5c54407e6679d87271173e15d170
Matheus-Silva7 <mate.almeidasilva@gmail.com>
2024-06-08 00:29:26 -0300

    services home responsive

--- src/components/services-/Services.jsx
+++ src/components/services-/Services.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { servicesData } from "../../data/servicesData";
+import "./ServicesStyles.css"
+import { IoIosAddCircle } from "react-icons/io";
 
 const Services = () => {
   return (
-    <section id="services" className="content-services">
+    <section id="services" className="container-services">
       <div className="text-services">
         <p>Services</p>
         <h2>Feel Like Home With Best Medical Care</h2>
       </div>
-      {servicesData.map((s, index) => (
-        <div className="cardServices" key={index}>
-          <img src={s.icon} alt="" />
-          <h3>{s.title}</h3>
-          <p>{s.desc}</p>
-          <a href="">ReadMore</a>
-        </div>
-      ))}
+      <div className="container-cards">
+        {servicesData.map((s, index) => (
+          <div className="cardServices" key={index}>
+            <img src={s.icon} alt="" />
+            <h3>{s.name}</h3>
+            <p>{s.desc}</p>
+            <a href="">ReadMore <IoIosAddCircle size={20} color="#C9C9C9"/> </a>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
